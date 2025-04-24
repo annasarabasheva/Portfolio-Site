13502436aa64eab1b9c858f750069a8ffe1cea6b
Anna Sarabasheva <annasarabasheva@gmail.com>
2025-04-24 19:29:03 +0300

    Add Header with Navigation

--- porfolio-site/src/App.jsx
+++ porfolio-site/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Home from './components/home/Home';
+import Header from './components/header/Header';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
+     <Header />
      <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
---
+++ porfolio-site/src/components/header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+        <nav>
+            <ul className="nav">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="#education">Education</Link></li>
+                <li><Link to="#word">Experience</Link></li>
+                <li><Link to="#getintouch">Get In Touch</Link></li>
+            </ul>
+        </nav>
+    </header>
+  );
+};
+
+export default Header;
