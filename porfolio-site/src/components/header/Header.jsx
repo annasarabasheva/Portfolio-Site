6856859d01f9f31307186027da265e91b2aab0ed
Anna Sarabasheva <annasarabasheva@gmail.com>
2025-05-04 23:44:24 +0300

    Add Projects section to Header nav

--- porfolio-site/src/components/header/Header.jsx
+++ porfolio-site/src/components/header/Header.jsx
@@ -11,6 +11,7 @@ const Header = () => {
                 <li><Link to="/education">Education</Link></li>
                 <li><Link to="/experience">Experience</Link></li>
                 <li><Link to="/skills"> Skills</Link></li>
+                <li><Link to="#projects"> Projects</Link></li>
                 <li><Link to="/contact">Contact</Link></li>
             </ul>
         </nav>
